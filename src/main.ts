fbaaef167b90f8a9d497a7926a758489131d9976
Ngac Duy Anh <ngac.duyanh@gmail.com>
2021-06-15 11:25:27 +0700

    fix: import correctly list item

--- src/main.ts
+++ src/main.ts
@@ -5,9 +5,11 @@ import store from "./store";
 import "./index.css";
 
 import BaseLayout from "@/components/BaseLayout.vue";
+import AppTaskListItem from "@/components/AppTaskListItem.vue";
 
 const app = createApp(App);
 
 app.component("BaseLayout", BaseLayout);
+app.component("AppTaskListItem", AppTaskListItem);
 
 app.use(store).use(router).mount("#app");
